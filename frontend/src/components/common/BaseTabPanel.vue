377b4171536f9e7eba471d829fd15ecbedc6798c
Paul Eeles <pauleeles@gmail.com>
2025-09-25 19:52:07 +0100

    feat: add reusable base tabs component

---
+++ frontend/src/components/common/BaseTabPanel.vue
@@ -0,0 +1,28 @@
+<template>
+    <section
+        v-show="id === active"
+        role="tabpanel"
+        :id="`panel-${id}`"
+        :aria-labelledby="`${labelledByPrefix}-${id}`"
+        class="h-full"
+    >
+        <slot />
+    </section>
+</template>
+
+<script setup>
+const props = defineProps({
+    id: {
+        type: [String, Number],
+        required: true,
+    },
+    active: {
+        type: [String, Number],
+        required: true,
+    },
+    labelledByPrefix: {
+        type: String,
+        default: 'tab',
+    },
+})
+</script>
